66e4bbb5d0368d7d23cd2ca5ef91f82089ccdd56
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-06-12 17:17:44 +0300

    Create MakingAnagrams.java

---
+++ MakingAnagrams.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MakingAnagrams {
+    public static void main(String[] args) {
+        String a = "cde";
+        String b = "abc";
+        char [] aChar = a.toCharArray();
+        char [] bChar = b.toCharArray();
+        Arrays.sort(aChar);
+        Arrays.sort(bChar);
+        for (int i = 0; i < aChar.length; i++) {
+
+        }
+    }
+}
